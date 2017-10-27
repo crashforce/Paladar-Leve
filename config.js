e90340534eefa691482f1a24169184ac5f99c45b
CrashForce <crashforce@gmail.com>
2017-10-27 01:46:56 -0200

    Envio do Mac
    Segundo envio

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'paladar-leve',
+		wp_ws_url : 'http://paladarleve.israelwilson.com.br/wp-appkit-api/paladar-leve',
+		wp_url : 'http://paladarleve.israelwilson.com.br',
+		theme : 'q-ios',
+		version : '0.1',
+		app_title : 'Paladar Leve',
+		app_platform : 'ios',
+		gmt_offset : -2,
+		debug_mode : 'off',
+		auth_key : '`(JGbWKroVvUYX5Ejz^y*X[)*Anh4DmRGh:eYL_qMA=bGf5P*!|TwN1t6]=[p+Of',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
